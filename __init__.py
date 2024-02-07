8724b202ad92202a70e81b6cb9af121539cbbde8
asd77170 <158565363+asd77170@users.noreply.github.com>
2024-02-07 14:43:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,10 +6,6 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
-@app.route('/')
-def hello_world():
-    return render_template('hello.html')
-  
 @app.route("/fr/")
 def monfr():
     return "<h2>Bonjour tout le monde !</h2>"
