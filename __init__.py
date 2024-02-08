ba85717799359c2b6884980fce80b910d5e1f2a8
asd77170 <158565363+asd77170@users.noreply.github.com>
2024-02-08 11:40:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -53,18 +53,7 @@ def Readfiche(post_id):
     return render_template('read_data.html', data=data)
 
 
-@app.route('/recherche_fiche_client/<string:post_Nom>')
-def Searchfile(post_Nom):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE Nom=?', (post_Nom,))
-    data = cursor.fetchall()
-    conn.close()
-    
 
-    
-    # Rendre le template HTML et transmettre les données
-    return render_template('read_data.html', data=data)
                                                                                      
 if __name__ == "__main__":
   app.run(debug=True)
