cc1d9dcceb62e43149dc1ec14602439af73126ab
asd77170 <158565363+asd77170@users.noreply.github.com>
2024-02-09 09:59:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -68,8 +68,8 @@ def formulaire_client():
     return render_template('formulaire_client.html')
 
 @app.route('/write/', methods=['POST'])
-def insert.py():
-    return render_template('insert.py')
+def write():
+    return (c'est par ici)
 
                   
 if __name__ == "__main__":
